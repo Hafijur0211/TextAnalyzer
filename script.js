2ae0afdfd45b746954b4242a80d4c12f4b7ec76a
Hafijur Rahman <hafijur021998@gmail.com>
2023-06-10 10:05:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("script work!")
+
+uppercase.addEventListener("click", () => {
+    console.log("Transforming to Uppercase")
+    inpText.value = inpText.value.toUpperCase()
+})
+
+lowercase.addEventListener("click", () => {
+    console.log("Transforming to Lowercase")
+    inpText.value = inpText.value.toLowerCase()
+})
+
+removespaces.addEventListener("click", () => {
+    console.log("Removing Extra Spaces..")
+    // https://stackoverflow.com/questions/16974664/how-to-remove-the-extra-spaces-in-a-string
+    inpText.value = inpText.value.replace(/\s+/g, ' ').trim();
+})
+
+removelines.addEventListener("click", () => {
+    console.log("Removing Extra Lines..")
+    // https://stackoverflow.com/questions/16974664/how-to-remove-the-extra-spaces-in-a-string
+    inpText.value = inpText.value.replace(/\n+/g, '\n').trim();
+})
+
+inpText.addEventListener("input", () => {
+    console.log("changed")
+    charCount.innerText = inpText.value.length
+    wordCount.innerText = inpText.value.trim().split(" ").length
+})
